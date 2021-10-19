7a74b6a1e6fc68af1e412e0c9a176ea556205729
Dosoy <olaf.meister@mail.ru>
2021-10-19 16:57:36 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+if __name__ == '__main__':
+    # my_list = []  # Пустой список
+    # my_list.append(1)  # добавляем единицу
+    # my_list.append(3)  # добавляем тройку
+    # print(my_list)
+
+    # my_list = []
+    # my_list.extend([1, 2, 3])  # [1, 2, 3]
+    # my_list.extend("add")  # [1, 2, 3, 'a', 'd', 'd']
+    # print(my_list)
+
+    # my_list = [1, 5]
+    # my_list.insert(1, 4)
+    # print(my_list)  # [1, 2, 3, 'a', 'd', 'd']
+
+    # my_list = [1, 5]
+    # my_list.remove(1)
+    # print(my_list)  # [5]
+
+    # L = ['red', 'green', 'blue']  # Remove 2nd list item
+    # L.pop(1)
+    # print(L)
+    # L = ['red', 'green', 'blue']
+    # x = L.pop(1)
+    # print(x)  # green
+
+    # Используйте метод count (), чтобы узнать, сколько раз указанный элемент появляется в списке.
+    # L = ['red', 'green', 'blue']
+    # print(L.count('red'))
+    # L = [1, 9, 7, 3, 9, 1, 9, 2]
+    # print(L.count(9))
+
+    # Метод index () ищет первое вхождение данного элемента и возвращает его индекс. Если указанный элемент не найден,
+    # возникает исключение ValueError.
+    # L = ['a', 'b', 'c', 'd', 'e', 'f', 'a', 'b', 'c', 'd', 'e', 'f']
+    # print(L.index('c'))
+
+    # L = [42, 99, 1, 12]
+    # L.sort()
+    # print(L)  # [1, 12, 42, 99]
+
+    # L = [1, 2, 3, 4, 5]
+    # L.reverse()
+    # print(L)  # [5, 4, 3, 2, 1]
+
+    # L = ['red', 'green', 'blue']
+    # X = L.copy()
+    # print(X)
+
+    L = ['red', 'green', 'blue']
+    L.clear()
+    print(L)  # []
+
+
